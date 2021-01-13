ddb8fea1e60087b8a87bd055121996132fe9fdff
suni <sun-918@naver.com>
2021-01-13 13:53:30 +0900

    Create

---
+++ SNGridCollectionView/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SNGridCollectionView
+//
+//  Created by suni on 2021/01/12.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
